ade4df64a7c5e1a914e2aab8fd77d3fcd1c2a6f7
MANIKJATANA <97248706+MANIKJATANA@users.noreply.github.com>
2022-02-26 09:25:12 +0530

    Add files via upload

---
+++ assignment 4/output_assign_4.py
@@ -0,0 +1,88 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============================================================= RESTART: C:\Users\manik\OneDrive\Desktop\assignment 4\code.py ============================================================
+                                                      ASSIGNMENT 4 
+
+
+
+
+QUESTION 1
+Move disk 1 from rod A to rod C
+Move disk 2 from rod A to rod B
+Move disk 1 from rod C to rod B
+Move disk 3 from rod A to rod C
+Move disk 1 from rod B to rod A
+Move disk 2 from rod B to rod C
+Move disk 1 from rod A to rod C
+
+
+QUESTION 2
+Enter the size of pascals triangle 5
+by using recursion 
+        1   
+      1   1   
+    1   2   1   
+  1   3   3   1   
+1   4   6   4   1   
+
+
+ with the help of loops 
+            1    
+          1    1    
+        1    2    1    
+      1    3    3    1    
+    1    4    6    4    1    
+
+
+
+
+QUESTION 3
+PART A 
+ENTER the first number-12
+ENTER the second number-21
+The quotient is- 0
+The remainder is- 12
+[0, 12]
+Callable- True
+PART B
+a is zero- False
+b is zero- False
+quotient is zero- True
+remainder is zero- False
+PART C
+[0, 12, 4, 5, 6]
+The values greater than 4- [12, 5, 6]
+PART D
+{12, 5, 6}
+PART E
+The required immutable set IS         frozenset({12, 5, 6})
+   PART F 
+The required max value is- 12
+The required hash value is- 12
+
+
+
+
+QUESTION 4
+MANIK JATANA 
+21103071
+
+
+
+
+QUESTION 5 
+Destructor called,The object is destroyed.
+part a 
+<a> The updated salary of Mehak is 50000 
+part b 
+Record of Viren have deleted sucessfully 
+
+
+
+
+QUESTION 6 
+Please give a word  mani
+Give a word-  na
+Possible Words- ['mani', 'amni', 'anmi', 'anim', 'mnai', 'nmai', 'nami', 'naim', 'mnia', 'nmia', 'nima', 'niam', 'main', 'amin', 'aimn', 'ainm', 'mian', 'iman', 'iamn', 'ianm', 'mina', 'imna', 'inma', 'inam']
+Friendship is fake.
